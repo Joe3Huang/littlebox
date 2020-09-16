ca3e3245f45fd84caf2837a365cf8d99006ffa9e
joe3huang <joe3huang@gmail.com>
2020-09-16 22:24:49 +1200

    added user model and error handler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { serve, ServerRequest } from 'https://deno.land/std@0.62.0/http/server.ts';
 import { Status } from 'https://deno.land/std@0.62.0/http/http_status.ts';
 
-import { getUsers, getUser } from './services/user.service.ts';
+import { getUsers, getUserById } from './services/user.service.ts';
 import { Route, HttpMethod, RouteParam, extractRouteParams, routeParamPattern } from './services/router.service.ts';
 
 const s = serve({ port: 8000 });
@@ -11,13 +11,13 @@ console.log(`server running on port 8000`);
 const routes: Route[] = [
   {
     method: HttpMethod.GET,
-    route: '/api/user',
+    route: '/api/v1/users',
     handler: getUsers,
   },
   {
     method: HttpMethod.GET,
-    route: '/api/user/:id',
-    handler: getUser,
+    route: '/api/v1/users/:id',
+    handler: getUserById,
   },
 ];
 
@@ -39,38 +39,64 @@ export const routeWithParamsRouteMatcher = (req: ServerRequest, route: Route): b
   return req.method === route.method && route.route.includes('/:') && routeMatcherRegEx.test(req.url);
 };
 
+class Error
+{
+    public status: Status = Status.ExpectationFailed;
+    public message: string = "Internal error";
+
+    public static fromErrorObj(obj: any): Error
+    {
+        const e = new Error();
+ 
+        if((typeof obj.message == 'string'))
+        {
+          console.log(12312);
+          e.message = obj.message; 
+        }
+        return e;
+    }
+}
+
 /**
  * Match the received request to a route in the routing table. return the handler function for that route
  * @param routes the routing table for the API
  * @param req the received request
  */
 export const matchRequestToRouteHandler = async (routes: Route[], req: ServerRequest): Promise<void> => {
-  let route: Route | undefined = routes.find((route: Route) => basicRouteMatcher(req, route));
-  if (route) {
-    const response: any = await route.handler();
-    return req.respond({ status: Status.OK, body: JSON.stringify(response) });
-  }
-  route = routes.find((route: Route) => routeWithParamsRouteMatcher(req, route));
-  if (route) {
-    // the received route has route params, extract the route params from the route
-    const routeParamsMap: RouteParam[] = extractRouteParams(route.route);
-    const routeSegments: string[] = req.url.split('/');
-    const routeParams: { [key: string]: string | number } = routeParamsMap.reduce(
-      (accum: { [key: string]: string | number }, curr: RouteParam) => {
-        return {
-          ...accum,
-          [curr.paramKey]: routeSegments[curr.idx],
-        };
-      },
-      {}
-    );
-    const response: any = await route.handler(...Object.values(routeParams));
-    return req.respond({ status: Status.OK, body: JSON.stringify(response) });
-  }
-  // route could not be found, return 404
-  return req.respond({ status: Status.NotFound, body: 'Route not found' });
+    try 
+    {
+        let route: Route | undefined = routes.find((route: Route) => basicRouteMatcher(req, route));
+        if (route) {
+            const response: any = await route.handler();
+            return req.respond({ status: Status.OK, body: JSON.stringify(response) });
+        }
+        route = routes.find((route: Route) => routeWithParamsRouteMatcher(req, route));
+        if (route) {
+            // the received route has route params, extract the route params from the route
+            const routeParamsMap: RouteParam[] = extractRouteParams(route.route);
+            const routeSegments: string[] = req.url.split('/');
+            const routeParams: { [key: string]: string | number } = routeParamsMap.reduce(
+            (accum: { [key: string]: string | number }, curr: RouteParam) => {
+                return {
+                ...accum,
+                [curr.paramKey]: routeSegments[curr.idx],
+                };
+            },
+            {}
+            );
+            const response: any = await route.handler(...Object.values(routeParams));
+            return req.respond({ status: Status.OK, body: JSON.stringify(response) });
+        }
+        // route could not be found, return 404
+        return req.respond({ status: Status.NotFound, body: 'Route not found' });
+    } 
+    catch (e)
+    {
+        const error = Error.fromErrorObj(e);
+        return req.respond({ status: error.status, body: error.message });
+    }
 };
 
 for await (const req: ServerRequest of s) {
-  matchRequestToRouteHandler(routes, req);
+    matchRequestToRouteHandler(routes, req);
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,25 +1,26 @@
-export async function getUser()
+import { Status } from 'https://deno.land/std@0.62.0/http/http_status.ts';
+import { User } from './../models/User.ts'
+
+const users = [
+    new User("1","Hello1","world2"),
+    new User("2","Hello2","world2"),
+    new User("3","Hello3","world2"),
+    new User("4","Hello4","world2"),
+    new User("5","Hello5","world2"),
+    new User("6","Hello6","world2")
+]
+
+export async function getUserById(id: string): Promise<User>
 {
-    return {
-        id: 123,
-        firstname: "Hello",
-        lastname: "world"
-    }
+    const theUser = users.find(u => u.id == id);
+    if(!theUser) throw { status: Status.NotFound, message: "User not found"}
+    return theUser;
 }
  
-export async function getUsers()
+export async function getUsers(Id: string) : Promise<User[]>
 {
-    const result = [
-        {
-            id: 123,
-            firstname: "Hello",
-            lastname: "world"
-        },
-        {
-            id: 1234,
-            firstname: "Hello1",
-            lastname: "world1"
-        }
-    ]
+    const result: User[] = [];
+    result.push(new User("123","Hello1","world2"));
+    result.push(new User("1234","Hello1","world2"));
     return result;
 }
